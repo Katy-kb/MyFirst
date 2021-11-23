0f01d356bab4819e3b0f259c43f6e279dbeafce9
unknown <katy-kb@yandex.ru>
2021-11-23 14:21:07 +0300

    first commit

---
+++ app/src/main/java/katy/kb/myfirstapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package katy.kb.myfirstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
